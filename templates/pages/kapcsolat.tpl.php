fd7ab42812fd30bcaa4ae0ae1f0c13ce00247f3d
Fekete Gergely <drafake@hotmail.com>
2022-12-01 17:10:17 +0100

    elérhetőség oldal

---
+++ templates/pages/kapcsolat.tpl.php
@@ -0,0 +1,21 @@
+<div class="col-sm-8">
+    <h1>Elérhetőségek</h1>
+    <p></p>
+<div id = "Madárkórház alapítvány">
+    <p><strong>Cím:</strong> 4071 Hortobágy, Petőfi tér 6.<p>
+    <p><strong>e-mail:</strong> madarkorhaz@gmail.com<p>
+    <p><strong>Adószám:</strong> 18557899-1-09<p>
+    <p><strong>Tel.:</strong> 0630/5-356-484<p>
+    <p><strong>Fax.:</strong> 0652/369-181<p>
+</div>
+
+    <div id = "Örökbefogadás">
+        <h4><strong>Örökbefogadással, honlappal kapcsolatos kérdések:</strong></h4>
+        <p><strong>e-mail: </strong> info@madarpark.hu <p>
+		<p><strong>Dr. Déri János:</strong><p>
+		<p>Tel: 0630/9-435-494<p>
+        <p><strong> Blog:</strong><p>
+		<a class="nav-link" href="https://madarpark.blog.hu/">Madárkórház Blog</a>
+        <p><strong> Facebook:</strong><p>
+		<a class="nav-link" href="https://www.facebook.com/madarpark">Madárkórház Facebook</a>
+    </div>
